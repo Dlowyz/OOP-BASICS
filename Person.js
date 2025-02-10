11d4dd79ab0f7391f727a96330221695e64c9447
Dlowyz <166107299+Dlowyz@users.noreply.github.com>
2025-02-10 13:03:16 +0200

    oopbasics

---
+++ Person.js
@@ -0,0 +1,30 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.birthYear = null;
+    }
+
+    setDateOfBirth(year) {
+        this.birthYear = year;
+    }
+
+    getDateOfBirth() {
+        return this.birthYear;
+    }
+
+    age() {
+        if (!this.birthYear) return null;
+        const currentYear = new Date().getFullYear();
+        return currentYear - this.birthYear;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    description() {
+        return `Person: ${this.name}`;
+    }
+}
+
+module.exports = Person;
